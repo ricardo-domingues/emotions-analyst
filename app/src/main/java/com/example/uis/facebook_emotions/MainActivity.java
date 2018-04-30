754aa5d9f5e8ed83d0ffc1de94360630c78da651
RodrigoSAlves <rodrigo.s.alves97@gmail.com>
2018-04-30 18:16:55 +0100

    Initial Spike with IBM Watson Tone Analyzer

--- app/src/main/java/com/example/uis/facebook_emotions/MainActivity.java
+++ app/src/main/java/com/example/uis/facebook_emotions/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.uis.facebook_emotions;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneScore;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements IBMCloudToneAnalyzerListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IBMCloudService.INSTANCE.analyzeText("My sister is a bitch. I hope she dies someday!", this);
+    }
+
+
+    @Override
+    public void onToneAnalyzerSuccess(List<ToneScore> scores) {
+        String detectedTones = "";
+        for(ToneScore score:scores) {
+            if(score.getScore() > 0.5f) {
+                detectedTones += score.getName() + " ";
+            }
+        }
+        final String toastMessage =
+                "The following emotions were detected:\n\n"
+                        + detectedTones.toUpperCase();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getContext(), toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onToneAnalyzerError(Exception e) {
+        e.printStackTrace();
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
     }
 }
